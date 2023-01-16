82aedab10716e415bca7b27c3e918749b9f096df
Lars95 <Lars.Wittkaemper@gmail.com>
2023-01-16 18:44:09 +0100

    feat(home): Uebersicht Ausleihen Home

--- knotnpunkt/views.py
+++ knotnpunkt/views.py
@@ -37,23 +37,25 @@ def redirectToLogin():
 @views.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-            return redirect(url_for('views.home'))
+        return redirect(url_for('views.home'))
     error = None
-    if request.method == 'POST': 
+    if request.method == 'POST':
         user = Benutzer.query.get(request.form['benutzername'])
         if user:
             if user.passwort == request.form['passwort'] or user.check_passwort(request.form['passwort']):
                 # TODO: Change login to hash-only by removing "or"-statement
-                debug(f'{user.benutzername} hat sich angemeldet, Rolle: {user.Rolle.name}')
+                debug(
+                    f'{user.benutzername} hat sich angemeldet, Rolle: {user.Rolle.name}')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
-            else: 
+            else:
                 debug(f"Falsches Passwort für {request.form['benutzername']}")
                 error = 'Benutzername oder Passwort falsch.'
         else:
-            debug(f"Benutzername {request.form['benutzername']} ist nicht in der Datenbank vorhanden")
+            debug(
+                f"Benutzername {request.form['benutzername']} ist nicht in der Datenbank vorhanden")
             error = 'Benutzername oder Passwort falsch.'
-    return render_template('login.html', error = error)
+    return render_template('login.html', error=error)
 
 
 @views.route('/logout', methods=['GET'])
@@ -70,16 +72,28 @@ def logout():
 @views.route('/home')
 @login_required
 def home():
-    return render_template('home.html', apps=current_user.views())
+    ausleihen = Ausleihe.query.filter_by(
+        ersteller_benutzername=current_user.benutzername).order_by(desc(Ausleihe.ts_von)).all()
+    ausleihen_filtered_future = []
+    ausleihen_filtered_past = []
+    for a in ausleihen:
+        ausleihe = {'empfaenger': a.empfaenger, "ts_von": a.ts_von,
+                    "ts_bis": a.ts_bis, "materialien": a.materialien.split(",")}
+        if a.ts_von > date.today():
+            ausleihen_filtered_future.append(ausleihe)
+        else:
+            ausleihen_filtered_past.append(ausleihe)
+    return render_template('home.html', apps=current_user.views(), ausleihen_zukunft=ausleihen_filtered_future, ausleihen_alt=ausleihen_filtered_past)
 
 
 @views.route("/benutzer", methods=['GET', 'POST'])
 @login_required
 def benutzer():
-    if request.method =='POST':
+    if request.method == 'POST':
         debug(request.form)
         debug(Rolle.query.filter_by(name="admin").first().idRolle)
-        neuerBenutzer = Benutzer(request.form.get('benutzername'), request.form.get('name'), request.form.get('email'), f"{request.form.get('benutzername')}", Rolle.query.filter_by(name=request.form.get('rolle')).first().idRolle)
+        neuerBenutzer = Benutzer(request.form.get('benutzername'), request.form.get('name'), request.form.get(
+            'email'), f"{request.form.get('benutzername')}", Rolle.query.filter_by(name=request.form.get('rolle')).first().idRolle)
         db.session.add(neuerBenutzer)
         db.session.commit()
         return redirect("/benutzer")
@@ -90,7 +104,7 @@ def benutzer():
             erlaubeBearbeiten = False
         liste = Benutzer.query.order_by(Benutzer.name).all()
         rollen = Rolle.query.all()
-        
+
         return render_template('benutzer.html', apps=current_user.views(), benutzer_liste=liste, roles=rollen, edit=erlaubeBearbeiten)
 
 
@@ -99,7 +113,7 @@ def benutzer():
 def profil(benutzername):
     fehlermeldung = ""
     if current_user.Rolle.lesenBenutzer or current_user.benutzername == benutzername:
-        if request.method== 'POST':
+        if request.method == 'POST':
             debug(request.form)
             user = Benutzer.query.get(benutzername)
             if request.form.get("delete", "off") == 'on':
@@ -108,11 +122,12 @@ def profil(benutzername):
                 debug("user gelöscht")
                 return redirect('/benutzer')
             else:
-                user.benutzername =  request.form['benutzername']
+                user.benutzername = request.form['benutzername']
                 user.name = request.form['name']
                 user.email = request.form['email']
                 if request.form.get('rolle'):
-                    user.rolleRef = Rolle.query.filter_by(name=request.form.get('rolle')).first().idRolle
+                    user.rolleRef = Rolle.query.filter_by(
+                        name=request.form.get('rolle')).first().idRolle
                 if request.form.get('passwort', None):
                     if request.form.get('passwort') == request.form.get('passwortBestaetigung'):
                         user.set_passwort(request.form.get('passwort'))
@@ -122,7 +137,7 @@ def profil(benutzername):
                     else:
                         fehlermeldung = "Bitte bestätige dein neues Passwort"
                 db.session.add(user)
-                db.session.commit()        
+                db.session.commit()
                 return redirect('/benutzer')
         else:
             user = Benutzer.query.get(benutzername)
@@ -131,7 +146,7 @@ def profil(benutzername):
                 erlaubeBearbeiten = True
             else:
                 erlaubeBearbeiten = False
-            return render_template('profil.html', apps=current_user.views(), user=user, roles = rollen, edit=erlaubeBearbeiten, error=fehlermeldung)
+            return render_template('profil.html', apps=current_user.views(), user=user, roles=rollen, edit=erlaubeBearbeiten, error=fehlermeldung)
     else:
         return Response(f'Du hast leider keinen Zugriff auf das Profil von {benutzername}.', 401)
 
@@ -146,10 +161,12 @@ def material():
         if request.form.get('rhArtNummer'):
             eigenschaften['rhArtNummer'] = request.form.get('rhArtNummer')
         debug(eigenschaften)
-        if int(eigenschaften['anzahl']) >1:
+        if int(eigenschaften['anzahl']) > 1:
             eigenschaften['verfuegbar'] = 1
-        eigenschaften['zuletztGescannt'] = dt.strftime(dt.now(), "%Y-%m-%d %H:%M")
-        neuesMaterial = Material(request.form.get('name'), request.form.get('kategorie'), json.dumps(eigenschaften))
+        eigenschaften['zuletztGescannt'] = dt.strftime(
+            dt.now(), "%Y-%m-%d %H:%M")
+        neuesMaterial = Material(request.form.get('name'), request.form.get(
+            'kategorie'), json.dumps(eigenschaften))
         db.session.add(neuesMaterial)
         db.session.commit()
         return redirect('/material')
@@ -160,20 +177,22 @@ def material():
         material_list = []
         for material in materialien:
             id = material.idMaterial
-            image = Img.query.filter_by(Material_idMaterial = id).first()
+            image = Img.query.filter_by(Material_idMaterial=id).first()
             if image != None:
-                material_list.append([material, base64.b64encode(image.img).decode('utf-8')])
+                material_list.append(
+                    [material, base64.b64encode(image.img).decode('utf-8')])
             else:
                 material_list.append([material, None])
-        return render_template('material.html', apps=current_user.views(), materialListe=material_list, kategorienListe=kategorien, verfuegbarkeit = verfuegbarkeit,  jsonRef=json, huRef=hu, dtRef=dt)
+        return render_template('material.html', apps=current_user.views(), materialListe=material_list, kategorienListe=kategorien, verfuegbarkeit=verfuegbarkeit,  jsonRef=json, huRef=hu, dtRef=dt)
 
 
 @views.route('/material/<idMaterial>', methods=['GET'])
 @login_required
 def materialDetails(idMaterial):
-    material_details = Material.query.filter_by(idMaterial = idMaterial).all()
+    material_details = Material.query.filter_by(idMaterial=idMaterial).all()
     materialien = Material.query.all()
-    ausleihen = Ausleihe.query.order_by(desc(Ausleihe.ts_von)).all() #Hier schon direkt Filtern ob MaterialID(Int) in Ausgeliehenem Material(Str) ist? 
+    # Hier schon direkt Filtern ob MaterialID(Int) in Ausgeliehenem Material(Str) ist?
+    ausleihen = Ausleihe.query.order_by(desc(Ausleihe.ts_von)).all()
     ausleihen_filtered_future = []
     ausleihen_filtered_past = []
     verfuegbarkeit = checkverfuegbarkeit(material_details)
@@ -184,24 +203,26 @@ def materialDetails(idMaterial):
             else:
                 ausleihen_filtered_past.append(a)
     if len(ausleihen_filtered_past):
-        zuletzt_ausgeliehen_Tage = (date.today() - ausleihen_filtered_past[0].ts_von).days
-    else: 
+        zuletzt_ausgeliehen_Tage = (
+            date.today() - ausleihen_filtered_past[0].ts_von).days
+    else:
         zuletzt_ausgeliehen_Tage = None
     kategorien = Kategorie.query.all()
-    material_images = Img.query.filter_by(Material_idMaterial = idMaterial).all()
+    material_images = Img.query.filter_by(Material_idMaterial=idMaterial).all()
     img_id_list = []
     if material_images:
         for image in material_images:
-            img_id_list.append([image.img_id, base64.b64encode(image.img).decode('utf-8')])
+            img_id_list.append(
+                [image.img_id, base64.b64encode(image.img).decode('utf-8')])
     else:
         img_id_list.append(None)
-    return render_template('material_details.html', apps=current_user.views(), material_details=material_details, materialListe = materialien, kategorienListe=kategorien, ausleihListeZukunft = ausleihen_filtered_future, ausleihListeAlt = ausleihen_filtered_past, verfuegbarkeit = verfuegbarkeit, zuletzt_ausgeliehen_Tage = zuletzt_ausgeliehen_Tage, jsonRef=json, huRef=hu, dtRef=dt, images = img_id_list)
+    return render_template('material_details.html', apps=current_user.views(), material_details=material_details, materialListe=materialien, kategorienListe=kategorien, ausleihListeZukunft=ausleihen_filtered_future, ausleihListeAlt=ausleihen_filtered_past, verfuegbarkeit=verfuegbarkeit, zuletzt_ausgeliehen_Tage=zuletzt_ausgeliehen_Tage, jsonRef=json, huRef=hu, dtRef=dt, images=img_id_list)
 
 
 @views.route('/material/edit/<idMaterial>', methods=['POST'])
 @login_required
 def materialDetailsEdit(idMaterial):
-    material_update = Material.query.filter_by(idMaterial = idMaterial).first()
+    material_update = Material.query.filter_by(idMaterial=idMaterial).first()
     material_update.name = request.form.get('name')
     material_update.Kategorie_idKategorie = request.form.get('kategorie')
     eigenschaften = material_update.Eigenschaften
@@ -210,7 +231,7 @@ def materialDetailsEdit(idMaterial):
     if request.form.get('rhArtNummer'):
         eigenschaften['rhArtNummer'] = request.form.get('rhArtNummer')
     if request.form.get('anzahl'):
-        if int(request.form.get('anzahl'))>1:
+        if int(request.form.get('anzahl')) > 1:
             eigenschaften['anzahl'] = int(request.form.get('anzahl'))
             eigenschaften['zaehlbar'] = True
         else:
@@ -224,13 +245,14 @@ def materialDetailsEdit(idMaterial):
 def materialReservieren(idMaterial):
     debug(request.form.get('reservierte_Materialien'))
     neueReservierung = Ausleihe(
-        ersteller_benutzername = current_user.benutzername,
-        ts_erstellt = dt.now(),
-        ts_von = dt.strptime(request.form.get('reservieren_von'), "%Y-%m-%d"), 
-        ts_bis = dt.strptime(request.form.get('reservieren_bis'), "%Y-%m-%d"),
-        materialien = request.form.get('reservierte_Materialien'),
-        empfaenger = request.form.get('empfaenger') if request.form.get('empfaenger') else current_user.benutzername,
-        beschreibung= request.form.get('beschreibung'))
+        ersteller_benutzername=current_user.benutzername,
+        ts_erstellt=dt.now(),
+        ts_von=dt.strptime(request.form.get('reservieren_von'), "%Y-%m-%d"),
+        ts_bis=dt.strptime(request.form.get('reservieren_bis'), "%Y-%m-%d"),
+        materialien=request.form.get('reservierte_Materialien'),
+        empfaenger=request.form.get('empfaenger') if request.form.get(
+            'empfaenger') else current_user.benutzername,
+        beschreibung=request.form.get('beschreibung'))
     db.session.add(neueReservierung)
     db.session.commit()
     return redirect('/material')
@@ -252,7 +274,7 @@ def upload_img(idMaterial):
     return redirect('/material/'+idMaterial)
 
 
-@views.route('/img/delete/<id>/<idMaterial>') #, methods=['POST']
+@views.route('/img/delete/<id>/<idMaterial>')  # , methods=['POST']
 @login_required
 def delete_img(id, idMaterial):
     Img.query.filter_by(img_id=id).delete()
@@ -277,13 +299,13 @@ def einstellungen():
 def scanner():
     return render_template('scanner.html')
 
+
 @views.route('/qrcode-generator', methods=['GET'])
 @login_required
 def qrcode_generator():
     materialien = Material.query.all()
     kategorien = Kategorie.query.all()
-    return render_template('qrgenerator.html', apps=current_user.views(), materialListe = materialien, kategorienListe=kategorien)
-
+    return render_template('qrgenerator.html', apps=current_user.views(), materialListe=materialien, kategorienListe=kategorien)
 
 
 # @views.route('/api/material')
