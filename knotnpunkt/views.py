0490c7c4a3a8c5c7e7c348656d13c523f9b3fea6
Lars95 <Lars.Wittkaemper@gmail.com>
2023-01-17 20:00:57 +0100

    feat(home): Carousel mit Ausleihen und Stats

--- knotnpunkt/views.py
+++ knotnpunkt/views.py
@@ -76,14 +76,30 @@ def home():
         ersteller_benutzername=current_user.benutzername).order_by(desc(Ausleihe.ts_von)).all()
     ausleihen_filtered_future = []
     ausleihen_filtered_past = []
+    stats_list = []
     for a in ausleihen:
+        materialien = []
+        for m in a.materialien.split(","):
+            materialien.append(Material.query.filter_by(idMaterial=m).first())
+            stats_list.append(m)
         ausleihe = {'empfaenger': a.empfaenger, "ts_von": a.ts_von,
-                    "ts_bis": a.ts_bis, "materialien": a.materialien.split(",")}
+                    "ts_bis": a.ts_bis, "materialien": materialien}
         if a.ts_von > date.today():
             ausleihen_filtered_future.append(ausleihe)
         else:
             ausleihen_filtered_past.append(ausleihe)
-    return render_template('home.html', apps=current_user.views(), ausleihen_zukunft=ausleihen_filtered_future, ausleihen_alt=ausleihen_filtered_past)
+    stats_dict = dict()
+    stats_dict1 = dict()
+    if len(stats_list) > 0:
+        for i in stats_list:
+            stats_dict[str(i)] = stats_dict.get(str(i), 0)+1
+        materialien = Material.query.all()
+        for m in materialien:
+            stats_dict1[m.name] = stats_dict.get(
+                str(m.idMaterial), 0) / len(stats_list)*100
+        max_value = max(stats_dict1.values())
+        print(stats_dict1)
+    return render_template('home.html', apps=current_user.views(), ausleihen_zukunft=ausleihen_filtered_future[:3], ausleihen_alt=ausleihen_filtered_past[:3], stats=stats_dict1, max=max_value)
 
 
 @views.route("/benutzer", methods=['GET', 'POST'])
