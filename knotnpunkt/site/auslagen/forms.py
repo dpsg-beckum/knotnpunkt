a42c0cf174dd35e980a56cafb7f8be4a5c25a09f
Jonah Wille <jonah.wille10@gmail.com>
2025-09-23 10:48:49 +0200

    feat(forms): add IBAN validation and formatting to NewAuslagenForm

--- knotnpunkt/site/auslagen/forms.py
+++ knotnpunkt/site/auslagen/forms.py
@@ -1,12 +1,36 @@
-import wtforms.widgets as widgets
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileField, MultipleFileField
-from werkzeug.datastructures.structures import ImmutableMultiDict
-from wtforms.fields import (DecimalField, SelectField, SelectMultipleField,
-                            StringField, SubmitField, TextAreaField)
-from wtforms.validators import DataRequired, EqualTo, Length, Optional, length
+import re
+
+from flask_wtf.file import FileAllowed, FileField
+from wtforms.fields import (DecimalField, SelectField, StringField,
+                            SubmitField, TextAreaField)
+from wtforms.validators import DataRequired, Length, Optional, ValidationError
 
 from ..forms import KPForm
+
+
+def validate_iban(form, field: StringField):
+    """Custom validator for IBAN format and checksum."""
+    value = field.data.replace(' ', '').upper()
+    # Basic IBAN format check
+    if not re.match(r'^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$', value):
+        raise ValidationError('Ungültiges IBAN-Format.')
+    # Move first four chars to end and convert letters to numbers (A=10, ..., Z=35)
+    rearranged = value[4:] + value[:4]
+    numerized = ''
+    for c in rearranged:
+        if c.isdigit():
+            numerized += c
+        elif c.isalpha():
+            numerized += str(ord(c) - 55)
+        else:
+            raise ValidationError('Ungültige Zeichen in der IBAN.')
+    # Perform mod-97 check
+    if int(numerized) % 97 != 1:
+        raise ValidationError('IBAN-Prüfziffer ist ungültig.')
+
+    # Save the formatted IBAN back to the field
+    field.data = value
+    return True
 
 
 class ShowAuslagenForm(KPForm):
@@ -39,7 +63,14 @@ class NewAuslagenForm(KPForm):
     kontoinhaber = StringField("Kontoinhaber_in *", validators=[
         DataRequired(), Length(max=100)])
     iban = StringField("IBAN *", validators=[
-        DataRequired(), Length(max=34)])
+        DataRequired(), Length(max=34), validate_iban])
     bic = StringField("BIC *", validators=[
         Optional(), Length(max=11)])
     submit = SubmitField("Einreichen")
+
+    def update_form(self):
+        super().update_form()
+
+        # format iban data to insert spaces every 4 chars
+        self.iban.data = ' '.join(
+            self.iban.data[i:i+4] for i in range(0, len(self.iban.data), 4))
